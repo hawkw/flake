ecd5d5ba7fcaa31e4afd6b8e5989d297d8927532
Eliza Weisman <eliza@elizas.website>
2024-03-26 09:01:48 -0700

    move TPM udev rules to a file

--- modules/nixos/profiles/tpm.nix
+++ modules/nixos/profiles/tpm.nix
@@ -1,13 +1,27 @@
 # Configuration to enable TPM on machines that have one.
-{ lib, config, ... }:
+{ lib, config, pkgs, ... }:
 let cfg = config.profiles.tpm;
-in {
-  options.profiles.tpm = with lib; { enable = mkEnableOption "TPM profile"; };
+in with lib; {
+  options.profiles.tpm = { enable = mkEnableOption "TPM profile"; };
 
-  config = lib.mkIf cfg.enable (
+  config = mkIf cfg.enable (
     let
       tss = "tss";
       uhid = "uhid";
+      tpmRules = pkgs.writeTextFile {
+        name = "tpm-udev-rules";
+        text = ''
+          # tpm devices can only be accessed by the tss user but the tss
+          # group members can access tpmrm devices
+          KERNEL=="tpm[0-9]*", TAG+="systemd", MODE="0660", OWNER="${tss}"
+          KERNEL=="tpmrm[0-9]*", TAG+="systemd", MODE="0660", OWNER="${tss}", GROUP="${tss}"
+
+          # uhid group can access /dev/uhid
+          KERNEL=="${uhid}", SUBSYSTEM=="misc", MODE="0660", GROUP="${uhid}"
+        '';
+        destination = "/etc/udev/rules.d/99-tpm.rules";
+      };
+
     in
     {
       boot.kernelModules = [ uhid ];
@@ -17,15 +31,7 @@ in {
 
       users.users.eliza.extraGroups = [ tss uhid ];
 
-      services.udev.extraRules = ''
-        # tpm devices can only be accessed by the tss user but the tss
-        # group members can access tpmrm devices
-        KERNEL=="tpm[0-9]*", TAG+="systemd", MODE="0660", OWNER="${tss}"
-        KERNEL=="tpmrm[0-9]*", TAG+="systemd", MODE="0660", OWNER="${tss}", GROUP="${tss}"
-
-        # uhid group can access /dev/uhid
-        KERNEL=="${uhid}", SUBSYSTEM=="misc", MODE="0660", GROUP="${uhid}"
-      '';
+      services.udev.packages = [ tpmRules ];
     }
   );
 }
