d9ed0c3b8f6febef149cc2e49c0f81ea840c6b38
Eliza Weisman <eliza@elizas.website>
2024-05-15 13:08:34 -0700

    lokiiiii

--- modules/nixos/profiles/observability/default.nix
+++ modules/nixos/profiles/observability/default.nix
@@ -373,63 +373,60 @@ in
                     path_prefix = dataDir;
                   };
 
-                  ingester = {
-                    # Any chunk not receiving new logs in this time will be flushed
-                    chunk_idle_period = "1h";
-                    # All chunks will be flushed when they hit this age, default is 1h
-                    max_chunk_age = "1h";
-                    # Loki will attempt to build chunks up to 1.5MB, flushing first if
-                    # chunk_idle_period or max_chunk_age is reached first
-                    chunk_target_size = 1048576;
-                    # Must be greater than index read cache TTL if using an index cache
-                    # (Default index read cache TTL is 5m)
-                    chunk_retain_period = "30s";
-                    # # Chunk transfers disabled
-                    # max_transfer_retries = 0;
-                  };
+                  # ingester = {
+                  #   # Any chunk not receiving new logs in this time will be flushed
+                  #   chunk_idle_period = "1h";
+                  #   # All chunks will be flushed when they hit this age, default is 1h
+                  #   max_chunk_age = "1h";
+                  #   # Loki will attempt to build chunks up to 1.5MB, flushing first if
+                  #   # chunk_idle_period or max_chunk_age is reached first
+                  #   chunk_target_size = 1048576;
+                  #   # Must be greater than index read cache TTL if using an index cache
+                  #   # (Default index read cache TTL is 5m)
+                  #   chunk_retain_period = "30s";
+                  #   # # Chunk transfers disabled
+                  #   # max_transfer_retries = 0;
+                  # };
 
-                  schema_config =
+                  schema_config.configs = [
                     {
-                      configs = [
-                        {
-                          from = "2020-01-01";
-                          store = "boltdb-shipper";
-                          object_store = "filesystem";
-                          schema = "v13";
-                          index = {
-                            prefix = "index_";
-                            period = "168h";
-                          };
-                        }
-                      ];
-                    };
+                      from = "2020-01-01";
+                      store = "tsdb";
+                      object_store = "filesystem";
+                      schema = "v13";
+                      index = {
+                        prefix = "index_";
+                        period = "168h";
+                      };
+                    }
+                  ];
 
                   storage_config = {
-                    boltdb_shipper = {
-                      active_index_directory = "${dataDir}/boltdb-shipper-active";
-                      cache_location = "${dataDir}/boltdb-shipper-cache";
-                      # Can be increased for faster performance over longer query
-                      # periods, uses more disk space
-                      cache_ttl = "24h";
-                    };
+                    # boltdb_shipper = {
+                    #   active_index_directory = "${dataDir}/boltdb-shipper-active";
+                    #   cache_location = "${dataDir}/boltdb-shipper-cache";
+                    #   # Can be increased for faster performance over longer query
+                    #   # periods, uses more disk space
+                    #   cache_ttl = "24h";
+                    # };
                     filesystem = {
                       directory = "${dataDir}/chunks";
                     };
                   };
 
-                  limits_config = {
-                    reject_old_samples = true;
-                    reject_old_samples_max_age = "168h";
-                  };
+                  # limits_config = {
+                  #   reject_old_samples = true;
+                  #   reject_old_samples_max_age = "168h";
+                  # };
 
                   # chunk_store_config = {
                   #   max_look_back_period = "0s";
                   # };
 
-                  table_manager = {
-                    retention_deletes_enabled = false;
-                    retention_period = "0s";
-                  };
+                  # table_manager = {
+                  #   retention_deletes_enabled = false;
+                  #   retention_period = "0s";
+                  # };
                 };
               };
             }
