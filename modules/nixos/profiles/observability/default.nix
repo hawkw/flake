b607cd46501d85b96ee5ad9d298d60095a8c38ba
Eliza Weisman <eliza@elizas.website>
2024-05-15 11:59:43 -0700

    observability: fixup promtail things that are sets

--- modules/nixos/profiles/observability/default.nix
+++ modules/nixos/profiles/observability/default.nix
@@ -97,29 +97,27 @@ in
             grpc_listen_port = 0;
           };
 
-          positions = {
-            filename = "/tmp/positions.yaml";
-          };
-
-          clients = {
+          clients = [{
             url = "http://noctis.local:${toString cfg.loki.port}/loki/api/v1/push";
-          };
+          }];
 
-          scrape_configs = {
-            job_name = "journal";
-            journal = {
-              max_age = "12h";
-              labels = {
-                job = "systemd-journal";
-                host = "${config.networking.hostName}";
+          scrape_configs = [
+            {
+              job_name = "journal";
+              journal = {
+                max_age = "12h";
+                labels = {
+                  job = "systemd-journal";
+                  host = "${config.networking.hostName}";
+                };
               };
-            };
 
-            relabel_configs = {
-              source_labels = [ "__journal__systemd_unit" ];
-              target_label = "unit";
-            };
-          };
+              relabel_configs = [{
+                source_labels = [ "__journal__systemd_unit" ];
+                target_label = "unit";
+              }];
+            }
+          ];
         };
       };
     })
