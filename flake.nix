fe7c2867a339f0c859e8b15a28e61d701038e089
Eliza Weisman <eliza@elizas.website>
2024-06-10 09:18:36 -0700

    add cachix and garnix to flake nixConfig

--- flake.nix
+++ flake.nix
@@ -1,5 +1,18 @@
 {
   description = "Eliza's NixOS and Home-Manager configuration flake";
+  ############################################################################
+  #### NIX CONFIG ############################################################
+  nixConfig = {
+    extra-substituters = [
+      "https://nix-community.cachix.org"
+      "https://cache.garnix.io"
+    ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+    ];
+  };
+
 
   ############################################################################
   #### INPUTS ################################################################
