67499118604aa1fee4842155e74bef5dc1e402e5
Eliza Weisman <eliza@elizas.website>
2024-06-17 12:35:42 -0700

    ok fine let's just not do this

--- modules/home/ssh.nix
+++ modules/home/ssh.nix
@@ -1,10 +1,9 @@
-{ config, lib, pkgs, ... }:
+{ config, lib, ... }:
 let
   _1passwordAgent = {
     enable = config.programs._1password-gui.enableSshAgent;
     path = "${config.home.homeDirectory}/.1password/agent.sock";
   };
-  authSockPath = "${config.home.homeDirectory}/.ssh/ssh_auth_sock";
 in
 with lib;
 {
@@ -12,27 +11,13 @@ with lib;
     mkEnableOption "Enable 1Password SSH Agent";
 
   config = mkIf _1passwordAgent.enable {
-    # home.file.".ssh/rc".source = pkgs.writeScript "ssh-agent-rc" ''
-    #   # create/update symlink only if interactive ssh login AND ~/.ssh/ssh_auth_sock doesn't exist AND $SSH_AUTH_SOCK does exist
-    #   if [[ -n "$SSH_TTY" && ! -S ${authSockPath} && -S "$SSH_AUTH_SOCK" ]]; then
-    #       ln -sf $SSH_AUTH_SOCK ${authSockPath}
-    #   fi
-    # '';
     programs.ssh = {
       enable = true;
       forwardAgent = _1passwordAgent.enable;
       addKeysToAgent = "yes";
       matchBlocks = {
-        # hasAuthSock = {
-        #   match = ''host * exec "test -S ${authSockPath}"'';
-        #   extraOptions = {
-        #     IdentityAgent = authSockPath;
-        #   };
-        # };
-        # noAuthSock = hm.dag.entryAfter [ "hasAuthSock" ] {
-
-        authSock = {
-          match = "host *";
+        "notSsh" = {
+          match = ''host * exec "test -z $SSH_CONNECTION"'';
           extraOptions = {
             IdentityAgent = _1passwordAgent.path;
           };
