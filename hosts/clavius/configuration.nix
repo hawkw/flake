795f167573f22235637b8b03f9b41659d8fbe7fa
Eliza Weisman <eliza@elizas.website>
2024-07-03 10:02:13 -0700

    roll eclssd, enable readoutd

--- hosts/clavius/configuration.nix
+++ hosts/clavius/configuration.nix
@@ -9,5 +9,8 @@
   hardware.raspberry-pi."4".apply-overlays-dtmerge.enable = true;
   profiles.eclss-node.enable = true;
   profiles.raspberry-pi.poe-hat.enable = true;
-  services.eclssd.location = "office";
+  services.eclssd = {
+    location = "office";
+    readoutd.ssd1680.enable = true;
+  };
 }
