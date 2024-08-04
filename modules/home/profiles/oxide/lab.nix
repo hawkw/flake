c89d828e4fd20616d26d9fd0d849eaecb51e3bf8
Eliza Weisman <eliza@elizas.website>
2024-08-04 11:58:56 -0700

    add london, excessive golfing

--- modules/home/profiles/oxide/lab.nix
+++ modules/home/profiles/oxide/lab.nix
@@ -7,87 +7,89 @@ with lib; {
   config = mkIf cfg.enable {
     programs.ssh.matchBlocks =
       let
-        switchZoneHostname = "fe80::aa40:25ff:fe05:602%%madrid_sw1tp0";
         vpn = "vpn.eng.oxide.computer";
-        madrid = "madrid";
-        madrid-all = "${madrid}-all";
-        madridgc = "${madrid}gc";
         jeeves = "jeeves";
         mkLabMachine = name: {
           host = name;
           hostname = "${name}.eng.oxide.computer";
-          proxyJump = "${vpn}";
+          proxyJump = vpn;
           user = "eliza";
         };
+        mkRacklette = { name, gzIp, switchZoneIp, }: (
+          let all = "${name}-all"; in {
+            #
+            # root in the global zone of the scrimlet:
+            #
+            "${name}gz" = hm.dag.entryBefore [ all ] {
+              host = "${name}gz";
+              hostname = "${gzIp}%%${name}_host0";
+            };
+
+            #
+            # Wicket in the switch zone:
+            #
+            "${name}wicket" = hm.dag.entryBefore [ all ] {
+              host = "${name}wicket";
+              hostname = "${switchZoneIp}%%${name}_sw1tp0";
+              user = "wicket";
+            };
+
+            #
+            # root in the switch zone:
+            #
+            "${name}switch" = hm.dag.entryBefore [ all ] {
+              host = "${name}switch";
+              hostname = switchZoneHostname;
+            };
+
+            #
+            # gimlets by cubby number
+            #
+            ${name} = hm.dag.entryBefore [ all jeeves ] {
+              host = "${name}gc*";
+              proxyCommand = ''ssh ${jeeves} pilot -r ${name} tp nc any $(echo "%h" | sed s/${name}gc//) %p'';
+              forwardAgent = true;
+              proxyJump = vpn;
+              extraOptions = {
+                ServerAliveInterval = "15";
+              };
+            };
+
+            #
+            # config applied to all of the above:
+            #
+            "${name}-all" = hm.dag.entryBefore [ jeeves ] {
+              host = "${name}*";
+              user = "root";
+              proxyJump = jeeves;
+              extraOptions = {
+                # Every time the racklet is reset, the host key changes, so
+                # silence all  of openssh's warnings that "SOMEONE MIGHT BE
+                # DOING SOMETHING NASTY".
+                StrictHostKeyChecking = "no";
+                UserKnownHostsFile = "/dev/null";
+                LogLevel = "error";
+              };
+            };
+          }
+        );
       in
-      {
-        ###
-        ### lab machines
-        ###
-        ${jeeves} = hm.dag.entryAfter [ madridgc ] (mkLabMachine jeeves);
-        atrium = mkLabMachine "atrium";
-        cadbury = mkLabMachine "cadbury";
-        yuban = mkLabMachine "yuban";
-        lurch = mkLabMachine "lurch";
+      ### lab machines
+      (attrsets.genAttrs [ jeeves "atrium" "cadbury" "yuban" "lurch" ] mkLabMachine)
 
-        ###
-        ### madrid
-        ###
+      ### racklette: madrid
+      // mkRacklette {
+        name = "madrid";
+        gzIp = "fe80::eaea:6aff:fe09:7f66";
+        switchZoneIp = "fe80::aa40:25ff:fe05:602";
+      }
 
-        #
-        # root in the global zone of the scrimlet:
-        #
-        "${madrid}gz" = hm.dag.entryBefore [ madrid-all ] {
-          host = "${madrid}gz";
-          hostname = "fe80::eaea:6aff:fe09:7f66%%${madrid}_host0";
-        };
-
-        #
-        # Wicket in the switch zone:
-        #
-        "${madrid}wicket" = hm.dag.entryBefore [ madrid-all ] {
-          host = "${madrid}wicket";
-          hostname = switchZoneHostname;
-          user = "wicket";
-        };
-
-        #
-        # root in the switch zone:
-        #
-        "${madrid}switch" = hm.dag.entryBefore [ madrid-all ] {
-          host = "${madrid}switch";
-          hostname = switchZoneHostname;
-        };
-
-        #
-        # madrid gimlets by cubby number
-        #
-        ${madridgc} = hm.dag.entryBefore [ "${madrid}-all" ] {
-          host = "${madridgc}*";
-          proxyCommand = ''ssh ${jeeves} pilot -r ${madrid} tp nc any $(echo "%h" | sed s/${madridgc}//) %p'';
-          forwardAgent = true;
-          proxyJump = vpn;
-          extraOptions = {
-            ServerAliveInterval = "15";
-          };
-        };
-
-        #
-        # config applied to all of the above:
-        #
-        "${madrid}-all" = hm.dag.entryBefore [ jeeves ] {
-          host = "${madrid}*";
-          user = "root";
-          proxyJump = jeeves;
-          extraOptions = {
-            # Every time Madrid is reset, the host key changes, so silence all
-            # of openssh's warnings that "SOMEONE MIGHT BE DOING SOMETHING
-            # NASTY".
-            StrictHostKeyChecking = "no";
-            UserKnownHostsFile = "/dev/null";
-            LogLevel = "error";
-          };
-        };
+      ### racklette: london
+      // mkRacklette {
+        name = "london";
+        gzIp = "fe80::eaea:6aff:fe09:8567";
+        switchZoneIp = "fe80::aa40:25ff:fe05:702";
       };
+
   };
 }
