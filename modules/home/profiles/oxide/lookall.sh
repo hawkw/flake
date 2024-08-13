b54926e66baf4992971f87f69c36b5266fe2d0aa
Eliza Weisman <eliza@elizas.website>
2024-08-13 12:12:36 -0700

    blurg

--- modules/home/profiles/oxide/lookall.sh
+++ modules/home/profiles/oxide/lookall.sh
@@ -3,11 +3,13 @@
 #
 # the first argument is the SSH host for the switch zone (e.g. madridswitch,
 # londonswitch, etc), and the remainder is passed into `looker`
-
+set -x
 host=$1
 shift
 
-ssh "$host" pilot host exec \
-    -c '/opt/oxide/oxlog/oxlog zones | grep nexus | xargs -L 1 /opt/oxide/oxlog/oxlog logs --current | xargs -L 1 cat' \
-    0-31 \
-    | looker "$@"
+# we disable SC2029 because shellcheck (rightly!) warns us that "$cmd" is expanded
+# on the client side, rather than on the server. but, we *want* that here -- we
+# want to forward the arguments to this script as a command to run on the remote
+# box.
+# shellcheck disable=SC2029
+ssh "$host" "pilot host exec -O -c '/opt/oxide/oxlog/oxlog zones | grep nexus | xargs -L 1 /opt/oxide/oxlog/oxlog logs --current | xargs -L 1 cat' 0-31 | looker $*"
