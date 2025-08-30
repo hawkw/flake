130b286ca1526787b0b2aa1fed28efd988b5c7c2
Eliza Weisman <eliza@elizas.website>
2025-08-30 12:04:45 -0700

    hekate: why the fuck did nixpkgs-fmt put whitespace there
    killing m yself

--- hosts/hekate/configuration.nix
+++ hosts/hekate/configuration.nix
@@ -150,7 +150,5 @@
   # and migrated your data accordingly.
   #
   # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
-  system.stateVersion = "
-      25.05
-      "; # Did you read the comment?
+  system.stateVersion = "25.05"; # Did you read the comment?
 }
