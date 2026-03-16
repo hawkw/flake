7814182ce9196a3f9ed11f3211e9faaa0686821e
Eliza Weisman <eliza@elizas.website>
2026-03-16 11:47:31 -0700

    modules/nixos/nix: oh so thats why my substituters were messed up

--- modules/nixos/nix.nix
+++ modules/nixos/nix.nix
@@ -36,7 +36,7 @@ with lib; {
           {
             trusted-users = [ "root" "eliza" ];
             extra-substituters = substituters;
-            trusted-substituters = substituters;
+            extra-trusted-substituters = substituters;
             extra-trusted-public-keys = [
               "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
               "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
