8770393440d456c1f728f60bbaba861c73c327b5
Eliza Weisman <eliza@elizas.website>
2026-08-08 11:01:23 -0700

    home: runCommandNoCC is deprecated

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -195,7 +195,7 @@ with lib; rec {
   # don't know why it doesn't work in `zsh.nix` off the top of my head, but I'm
   # too lazy to figure it out.
   xdg.configFile."zsh/vendor-completions".source = with pkgs;
-    runCommandNoCC "vendored-zsh-completions" { } ''
+    runCommand "vendored-zsh-completions" { } ''
       mkdir -p $out
       ${fd}/bin/fd -t f '^_[^.]+$' \
         ${lib.escapeShellArgs home.packages} \
