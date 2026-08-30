0a227a4d5dc3a77a12bfce70542206d377aedbb5
Eliza Weisman <eliza@elizas.website>
2026-08-30 11:13:33 -0700

    hosts/tranquility: also headless

--- hosts/tranquility/home.nix
+++ hosts/tranquility/home.nix
@@ -1,8 +1,11 @@
-{ ... }: {
+{ lib, ... }: {
   profiles = {
     opstools = {
       enable = true;
       net.enable = true;
     };
   };
+
+  # TODO(eliza): figure out which editor to use more smartly...
+  home.sessionVariables.EDITOR = lib.mkForce "nano"; # headless...
 }
